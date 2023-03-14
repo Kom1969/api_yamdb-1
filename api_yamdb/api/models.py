f8221dae36d5dffff63ea251ebdca723282f0b52
SmirEV <ev_smir@mail.ru>
2023-03-14 13:24:31 +0300

    Models and serializers

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,2 @@
 from django.db import models
-import time
 # Create your models here.
---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,24 @@
+﻿from review.models import Category, Genre, Title
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+        read_only_fields = ('__all__', )
