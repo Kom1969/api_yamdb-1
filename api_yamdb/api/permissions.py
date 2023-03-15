0b813626bcd438ff82698e61369e24a51108ec59
SmirEV <ev_smir@mail.ru>
2023-03-15 10:42:21 +0300

    Add permissions. GET work, POST doesnt work

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,14 @@
+﻿from rest_framework import permissions
+
+
+class ForAnybody(permissions.BasePermission):
+    """
+    Object-level permission to only allow authors of an object to edit it.
+    Assumes the model instance has an `author` attribute.
+    """
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return True
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
 from reviews.models import Category, Genre, Title
+from .permissions import ForAnybody
 from .serializers import (CategorySerializer, GenreSerializer,
                           TitleSerializer)
 
@@ -10,19 +11,28 @@ from .serializers import (CategorySerializer, GenreSerializer,
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (ForAnybody,)  # потом - IsAdminUser ???
     pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (ForAnybody, )  # (permissions.AllowAny,)
     pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    permission_classes = (permissions.AllowAny, )
+    permission_classes = (ForAnybody, )
     pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save()
