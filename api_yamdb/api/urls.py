17971fd665193b44505cb70c8fcfa823c9a71a7e
Michail Koryukin <kom1969@list.ru>
2023-03-17 13:22:13 +0500

    Исправление urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,9 +9,10 @@ v1_router.register('categories', CategoryViewSet, basename='category')
 v1_router.register('genres', GenreViewSet, basename='genre')
 v1_router.register('titles', TitleViewSet, basename='title')
 v1_router.register(r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
-                   basename='comments')
-v1_router.register(r'^titles/(?P<title_id>\d+)/comments', CommentViewSet,
-                   basename='comments')
+                   basename='reviews')
+v1_router.register(
+    r'^titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments')
 v1_router.register('users',
                    UserViewSet,
                    basename='users')
